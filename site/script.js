e99cd7e1e961417026aeab0cb1d49589496b1c20
goldstargloww <goldstargloww@gmail.com>
2024-02-25 12:15:53 -0500

    sitelen pona

--- site/script.js
+++ site/script.js
@@ -1,6 +1,8 @@
 // https://www.reddit.com/r/neocities/comments/10t6btl/comment/j7ajjqc
 
 let isEnglish;
+let isTokiPona;
+let isSitelenPona;
 
 function tokiponaLang() {
   for(let en of document.querySelectorAll('.txt-en'))
@@ -11,7 +13,13 @@ function tokiponaLang() {
   {
     tp.style.display = 'inline';
   }
+  for(let sp of document.querySelectorAll('.txt-sp'))
+  {
+    sp.style.display = 'none'
+  }
   isEnglish = false;
+  isTokiPona = true;
+  isSitelenPona = false;
 }
 function englishLang() {
   for(let en of document.querySelectorAll('.txt-en'))
@@ -22,8 +30,32 @@ function englishLang() {
   {
     tp.style.display = 'none';
   }
+  for(let sp of document.querySelectorAll('.txt-sp'))
+  {
+    sp.style.display = 'none'
+  }
   isEnglish = true;
+  isTokiPona = false;
+  isSitelenPona = false;
 }
+function sitelenponaLang() {
+  for(let en of document.querySelectorAll('.txt-en'))
+  {
+    en.style.display = 'none';
+  }
+  for(let tp of document.querySelectorAll('.txt-tp'))
+  {
+    tp.style.display = 'none';
+  }
+  for(let sp of document.querySelectorAll('.txt-sp'))
+  {
+    sp.style.display = 'inline'
+  }
+  isEnglish = false;
+  isTokiPona = false;
+  isSitelenPona = true;
+}
+
 window.onload = function () {
   englishLang();
 }
@@ -31,6 +63,9 @@ window.onload = function () {
 function toggleLang() {
   if(isEnglish) {
     tokiponaLang();
+  }
+  else if(isTokiPona) {
+    sitelenponaLang();
   }
   else {
     englishLang();
