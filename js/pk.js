56b7bdd80f85a17c3250e2e99a60a2ae429d339b
goldstargloww <goldstargloww@gmail.com>
2024-09-15 16:44:30 -0400

    reworked a lot of things - profiles now use flexbox - no more toki/sitelen pona because i'm not happy with how it works, i'll work on it later - css update it is now porple - assets folder now exists - fonts are in assets/fonts - javascript folder now exists - scripts are in /js

--- js/pk.js
+++ js/pk.js
@@ -1,13 +1,13 @@
 // code by greysdawn https://codepen.io/greysdawn/pen/ZEZXEzR
 
 
-// useful for making the change easy to see later
+// useful for debugging
 function sleep() {
     return new Promise((res, rej) => {
       setTimeout(() => res(), 1000);
     });
   }
-  
+
 async function changeAvatars() {
   // set your system id for easy referencing
   var sysid = "mttho";
@@ -19,25 +19,24 @@ async function changeAvatars() {
   // extract the data from the response
   var members = await resp.json();
 
-  // creating a pause so the change is easier to see
-  // ignore this!
-  await sleep();
+  // creating a pause so the change is easier to see - use for debugging
+  // await sleep();
 
   // handle the avatar changing
   for (var m of members) {
-    // grab element
-    var el = document.getElementById(`${m.id}-avatar`);
-    if (!el) continue; // doesn't exist? ignore it!
-    if (m.webhook_avatar_url) { // gold edit
-      el.src = m.webhook_avatar_url; // gold edit
-    } else if (m.avatar_url) { // gold edit
-      el.src = m.avatar_url; // or change it, if it does exist
+    var element = document.getElementById(`${m.id}-avatar`); // grab element
+
+    if (!element) continue; // doesn't exist? ignore it!
+    
+    if (m.webhook_avatar_url) {
+      element.src = m.webhook_avatar_url;
+    } else if (m.avatar_url) {
+      element.src = m.avatar_url; // or change it, if it does exist
     } else {
       continue
     }
   }
   console.log("all avatars changed!")
 }
-  
+
 changeAvatars();
-  
